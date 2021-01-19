c8e55e0afa88de742bce7874a63c1c60d46b8604
Carltz Fertil <40922301+ledney07@users.noreply.github.com>
2021-01-18 20:51:07 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCofabyaUAcngtNRbe2A2pp3r9f8kVl7hU",
+  apiKey: "AIz*******************************",
   authDomain: "clone-565af.firebaseapp.com",
   projectId: "clone-565af",
-  storageBucket: "clone-565af.appspot.com",
-  messagingSenderId: "175034865625",
-  appId: "1:175034865625:web:3352ea1ba7c58debbc6bc8",
-  measurementId: "G-GB59Y157BY",
+  storageBucket: "c>>>>>>>>>>>>>>>>>>>>",
+  messagingSenderId: "************",
+  appId: "1:17*****************************",
+  measurementId: ">>>>>>>>>>>>>>>>>>",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
